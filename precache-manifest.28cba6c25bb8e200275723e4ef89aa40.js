326834f3efe72cc7309b3ffebadaf25e3bbc3576
raja1205 <rajacool84@gmail.com>
2022-05-15 23:12:48 +0530

    Updates

---
+++ precache-manifest.28cba6c25bb8e200275723e4ef89aa40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "051627326416c2727492",
+    "url": "/weather-finder/static/css/main.36fcf58a.chunk.css"
+  },
+  {
+    "revision": "051627326416c2727492",
+    "url": "/weather-finder/static/js/main.05162732.chunk.js"
+  },
+  {
+    "revision": "c5b6cd0912990a9f84a7",
+    "url": "/weather-finder/static/js/runtime~main.c5b6cd09.js"
+  },
+  {
+    "revision": "01a2ff0b38275e572bdc",
+    "url": "/weather-finder/static/css/2.06e24127.chunk.css"
+  },
+  {
+    "revision": "01a2ff0b38275e572bdc",
+    "url": "/weather-finder/static/js/2.01a2ff0b.chunk.js"
+  },
+  {
+    "revision": "a8f9a350e36265c2fa9437b68869af24",
+    "url": "/weather-finder/static/media/bgg.a8f9a350.jpg"
+  },
+  {
+    "revision": "ea66953c9badd175254c95f657656866",
+    "url": "/weather-finder/static/media/bg.ea66953c.jpg"
+  },
+  {
+    "revision": "3f40b907483ec5db30d1d301b3f82ac6",
+    "url": "/weather-finder/index.html"
+  }
+];
